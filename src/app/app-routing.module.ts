264d002162068b6f3578b3b68d90f4421ba1c22c
Jieping Lu <jieping.lu@spr.com>
2019-06-06 09:56:42 -0500

    readme and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {CalculatorComponent} from './calculator/calculator.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/usatt_rating_calculator', pathMatch: 'full'},
+  {path: 'usatt_rating_calculator', component: CalculatorComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
